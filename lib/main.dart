7f5940fbfa47feba44f21d55dca6f24af743f6f2
mohammedshaneaf <mohammed.shaneaf.963@gmail.com>
2024-11-21 15:46:48 +0300

    init hive in main  and adding constant note

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app/constant.dart';
 import 'package:notes_app/views/notes_views.dart';
 
-void main() {
-  runApp(NotesApp());
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
+
+  runApp(const NotesApp());
 }
 
 class NotesApp extends StatelessWidget {
