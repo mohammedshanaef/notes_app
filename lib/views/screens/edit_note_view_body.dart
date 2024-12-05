b75e12828d5526654693e9678f2f58c32cc68fa1
mohammedshaneaf <mohammed.shaneaf.963@gmail.com>
2024-12-05 12:35:01 +0300

    adding cofetti to the button in edit note view

--- lib/views/screens/edit_note_view_body.dart
+++ lib/views/screens/edit_note_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:confetti/confetti.dart'; // استيراد المكتبة
 import 'package:notes_app/cubits/cubit/cubit/notes_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/widgets/custom_appBar.dart';
@@ -15,39 +16,76 @@ class EditNoteViewBody extends StatefulWidget {
 
 class _EditNoteViewBodyState extends State<EditNoteViewBody> {
   String? title, content;
+  bool isLodaing = false;
+  late ConfettiController _confettiController; // التحكم بالكونفيتي
+
+  @override
+  void initState() {
+    super.initState();
+    _confettiController = ConfettiController(duration: const Duration(seconds: 2)); // مدة الكونفيتي
+  }
+
+  @override
+  void dispose() {
+    _confettiController.dispose(); // تأكد من تحرير الموارد
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Stack(
       children: [
-        CustomAppBar(
-          onPressed: () {
-            widget.note.title = title ?? widget.note.title; // if title null return the back value'
-            widget.note.subtitle = content ?? widget.note.subtitle; // if subtitle null return the back value
-            widget.note.save(); // Save Method From Hive Object
-            BlocProvider.of<NotesCubit>(context).fetchAllNotes(); // Refresh The Data After Changes
-            Navigator.pop(context);
-          },
-          title: 'Edit Note',
-          icon: Icons.check,
+        Column(
+          children: [
+            CustomAppBar(
+              onPressed: () async {
+                setState(() {
+                  isLodaing = true;
+                });
+                await Future.delayed(const Duration(seconds: 1));
+                widget.note.title = title ?? widget.note.title;
+                widget.note.subtitle = content ?? widget.note.subtitle;
+                widget.note.save();
+                BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+                setState(() {
+                  isLodaing = false;
+                });
+                _confettiController.play(); // تشغيل الكونفيتي
+                await Future.delayed(const Duration(seconds: 2)); // مدة الكونفيتي
+                Navigator.pop(context);
+              },
+              title: 'Edit Note',
+              icon: isLodaing ? Icons.hourglass_top : Icons.check,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            CustomTextFormField(
+              onChange: (value) {
+                title = value;
+              },
+              hintText: widget.note.title,
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            CustomTextFormField(
+              onChange: (value) {
+                content = value;
+              },
+              hintText: widget.note.subtitle,
+              maxLines: 5,
+            ),
+          ],
         ),
-        const SizedBox(
-          height: 50,
-        ),
-        CustomTextFormField(
-          onChange: (value) {
-            title = value;
-          },
-          hintText: widget.note.title,
-        ),
-        const SizedBox(
-          height: 25,
-        ),
-        CustomTextFormField(
-          onChange: (value) {
-            content = value;
-          },
-          hintText: widget.note.subtitle,
-          maxLines: 5,
+        Align(
+          alignment: Alignment.topCenter,
+          child: ConfettiWidget(
+            confettiController: _confettiController,
+            blastDirectionality: BlastDirectionality.explosive, // اتجاه الكونفيتي
+            shouldLoop: false, // عدم التكرار
+            colors: const [Colors.blue, Colors.green, Colors.orange, Colors.pink],
+          ),
         ),
       ],
     );
